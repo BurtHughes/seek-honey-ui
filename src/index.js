be50fafc2c1431e002f4f12708e35f18ba605ab4
Burt Hughes <normanthompson@163.com>
2018-06-23 23:04:59 +0800

    创建UI工程

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Test extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {name: props.name};
+    }
+
+    render () {
+        return (
+            <h1>Hello, {this.state.name}!</h1>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Test name='Tom'/>,
+    document.getElementById('root')
+);
