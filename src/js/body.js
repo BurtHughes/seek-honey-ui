6db0a44c14e265d23a1a7c62586bc217a80d85a2
Burt Hughes <normanthompson@163.com>
2018-07-06 20:36:42 +0800

    新增微信网页授权

--- src/js/body.js
+++ src/js/body.js
@@ -6,7 +6,7 @@ class Body extends React.Component{
     render () {
         return (
             <div>
-                <Button text="点" href="http://mh98yj.natappfree.cc" />
+                <Button text="微信网页授权" href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxaf8ebc4b41979524&redirect_uri=http%3A%2F%2F6uwuep.natappfree.cc%2FauthSuccess&response_type=code&scope=snsapi_userinfo&state=yeah#wechat_redirect" />
             </div>
         );
     }
