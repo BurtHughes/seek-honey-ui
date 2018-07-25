d692ed88fa8949df15da4faa25b48382ff829b32
Burt Hughes <normanthompson@163.com>
2018-07-25 09:44:48 +0800

    格式化代码

--- src/js/common/request.js
+++ src/js/common/request.js
@@ -2,23 +2,31 @@ import 'whatwg-fetch';
 // Restfult风格请求
 let baseUrl = 'http://127.0.0.1/';
 let req = obj => {
-    fetch(baseUrl + obj.path,{
-        method: obj.method
-    })
-    .catch((err)=>{
-        console.log('fetch出错:' + err);
-    });
+    let path = '';
+    if (typeof obj.path === 'string') {
+        if (obj.path.indexOf('http') === 0) {
+            path = obj.path;
+        } else {
+            path = baseUrl + obj.path;
+        }
+    } else {
+        return;
+    }
+    fetch(path, { method: obj.method })
+        .catch((err) => {
+            console.error('请求出错:' + err);
+        });
 };
 
 export let GET = obj => {
-    req({...obj, method:'get'});
+    req({ ...obj, method: 'get' });
 };
 export let POST = obj => {
-    req({...obj, method:'post'});
+    req({ ...obj, method: 'post' });
 };
 export let PUT = obj => {
-    req({...obj, method:'put'});
+    req({ ...obj, method: 'put' });
 };
 export let DELETE = obj => {
-    req({...obj, method:'delete'});
+    req({ ...obj, method: 'delete' });
 };
--- src/js/home.js
+++ src/js/home.js
@@ -8,8 +8,8 @@ import {
     CellFooter,
     Toast
 } from 'react-weui';
-import {GET} from './common/request';
-import {TestUrl} from './test-data/url-list';
+import { GET } from './common/request';
+import { TestUrl } from './test-data/url-list';
 import fm1 from '../img/fm1.jpg';
 import 'whatwg-fetch';
 
@@ -22,48 +22,48 @@ export default class Home extends React.Component {
 
     constructor() {
         super();
-        fetch(TestUrl.productList,{method:'get'})
-        .then(res=>res.json())
-        .then(res=>{
-            setTimeout(()=>{
-                this.setState({dataList:res.data,showLoading:false});
-            },2000);
-        })
-        .catch(err=>{
-            console.log('fetch出错:' + err);
-        });
+        fetch(TestUrl.productList, { method: 'get' })
+            .then(res => res.json())
+            .then(res => {
+                setTimeout(() => {
+                    this.setState({ dataList: res.data, showLoading: false });
+                }, 2000);
+            })
+            .catch(err => {
+                console.log('fetch出错:' + err);
+            });
     }
 
     cellList = (list, props) => {
         let result;
-        for(let i in list) {
+        for (let i in list) {
             let cur = list[i];
-            result +=   <Cell href="javascript:;" onClick={props.popup} link>
-                            <CellHeader>
-                                <img src={fm1} alt="" style={{ width: '100px' }} />
-                            </CellHeader>
-                            <CellBody>蜜蜂1</CellBody>
-                            <CellFooter />
-                        </Cell>;
+            result += <Cell href="javascript:;" onClick={props.popup} link>
+                <CellHeader>
+                    <img src={fm1} alt="" style={{ width: '100px' }} />
+                </CellHeader>
+                <CellBody>蜜蜂1</CellBody>
+                <CellFooter />
+            </Cell>;
         }
         return result;
     }
 
     render = () => {
         return (
-            <div style={{display: this.props.display}}>
+            <div style={{ display: this.props.display }}>
                 <Toast icon="loading" show={this.state.showLoading}>Loading...</Toast>
                 <CellsTitle>新鲜的蜂蜜</CellsTitle>
                 <Cells>
                     {
-                        this.state.dataList.map(obj=>{
-                            return  <Cell href="javascript:;" onClick={this.props.popup} link>
-                                        <CellHeader>
-                                            <img src={fm1} alt={obj.imgId} style={{ width: '100px' }} />
-                                        </CellHeader>
-                                        <CellBody>obj.productName</CellBody>
-                                        <CellFooter />
-                                    </Cell>;
+                        this.state.dataList.map(obj => {
+                            return <Cell href="javascript:;" onClick={this.props.popup} link>
+                                <CellHeader>
+                                    <img src={fm1} alt={obj.imgId} style={{ width: '100px' }} />
+                                </CellHeader>
+                                <CellBody>obj.productName</CellBody>
+                                <CellFooter />
+                            </Cell>;
                         })
                     }
                 </Cells>
